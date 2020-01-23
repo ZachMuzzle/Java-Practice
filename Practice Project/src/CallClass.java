a58618cf0df7e348b0c42ee9020fd3c8508085f0
gearz <gearz@localhost>
2020-01-23 16:11:02 -0500

    Java Practice Push

---
+++ Practice Project/src/CallClass.java
@@ -0,0 +1,8 @@
+
+public class CallClass {
+	public static void Call(int method) {
+		
+		int called = 20 + method;
+		System.out.println(called);
+	}
+}
---
+++ Practice Project/src/Practice.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		test(20);
+		List<int[]> list = new ArrayList<int[]>();
+;		int[] arynum;
+		arynum = new int[6];
+		
+		arynum[0] = 10;
+		arynum[1] = 15;
+		arynum[2] = 20;
+		arynum[3] = 6;
+		arynum[4] = 8;
+		arynum[5] = 3;
+		for (int i=0; i < arynum.length; i++) {
+			System.out.print(" number: " + arynum[i] );
+		}
+		System.out.println("\n");
+		int[] array1 = {1,4, 7, 5, 3, 7};
+		for (int i=0; i<array1.length;i++) {
+			System.out.print(" Number: " + array1[i]);
+
+		}
+		System.out.println("\n");
+
+		//Array List added
+		list.add(arynum);
+		list.add(array1);
+		for(int[] array:list) {
+			System.out.println(Arrays.toString(array));
+		}
+	}
+	
+	static void test(int hello) {
+		
+		int number = 20 + hello;
+		System.out.println(number);
+		
+		CallClass.Call(number);
+		System.out.println(number);
+	}
+}
